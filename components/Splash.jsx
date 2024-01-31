f4393b7ce4c4022155817b13be0542c573d7b432
unknown <atte.marjamaki@arcada.fi>
2024-01-31 19:02:02 +0200

    remove fox

--- components/Splash.jsx
+++ components/Splash.jsx
@@ -60,7 +60,7 @@ function Splash() {
               target="_blank"
             >
               <p className=" hover:text-fb-white">
-                Click here to install Metamask 🦊
+                Click here to install Metamask
               </p>
             </Link>
           </div>
